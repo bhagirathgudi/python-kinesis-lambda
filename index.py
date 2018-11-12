50ea5ac74be01107c4ee86d316566a14b11785ec
bhagirathgudi <bhagirath009@hotmail.com>
2018-11-12 20:56:13 +0000

    Removed unnecessary files

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
+import boto3
+import uuid
 import json
-import datetime
 
+stream_name = 'sample-example'
+client = boto3.client('kinesis')
 
-def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+def lambda_handler(event, context):
+  partition_key = uuid.uuid4()
+  payload = {
+    'name':'BHagirath',
+    'timestamp': '2018-10-21',
+    'radomData':'sdadfafasdfsa'
+  }
+  
+  response = client.put_record(StreamName = stream_name,
+  Data= json.dumps(payload),
+  PartitionKey = partition_key)
+  print(response)
+  
+  
+
